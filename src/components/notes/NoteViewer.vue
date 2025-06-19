89fd86f8f1129564328937f3cd8295c71e133b23
Mikkel Andersen <35904625+Mikkelka@users.noreply.github.com>
2025-06-19 12:24:42 +0200

    Fix mobile NoteViewer - reorder action buttons and fix delete dialog z-index visibility

--- src/components/notes/NoteViewer.vue
+++ src/components/notes/NoteViewer.vue
@@ -108,9 +108,17 @@
         </div>
 
         <!-- Mobile Actions -->
-        <div v-if="!isEditing" class="p-4 border-t border-gray-700/50 space-y-3">
-          <!-- Action buttons row -->
+        <div v-if="!isEditing" class="p-4 border-t border-gray-700/50">
+          <!-- All action buttons in one row -->
           <div class="flex justify-center gap-4">
+            <BaseButton
+              @click="handleDelete"
+              variant="ghost"
+              size="sm"
+              class="p-2 rounded-full text-red-400 hover:bg-red-500/20"
+            >
+              <Trash2 class="w-5 h-5" />
+            </BaseButton>
             <BaseButton
               @click="$emit('toggleFavorite', note.id)"
               variant="ghost"
@@ -124,12 +132,12 @@
               />
             </BaseButton>
             <BaseButton
-              @click="handleDelete"
+              @click="startEdit"
               variant="ghost"
               size="sm"
-              class="p-2 rounded-full text-red-400 hover:bg-red-500/20"
+              class="p-2 rounded-full text-blue-400 hover:bg-blue-500/20"
             >
-              <Trash2 class="w-5 h-5" />
+              <Edit3 class="w-5 h-5" />
             </BaseButton>
             <BaseButton
               @click="$emit('close')"
@@ -140,15 +148,6 @@
               <X class="w-5 h-5" />
             </BaseButton>
           </div>
-          <!-- Edit button -->
-          <BaseButton
-            @click="startEdit"
-            variant="primary"
-            class="w-full bg-blue-600 hover:bg-blue-500 py-2"
-          >
-            <Edit3 class="w-4 h-4 mr-2" />
-            Rediger Note
-          </BaseButton>
         </div>
       </div>
     </div>
@@ -324,6 +323,7 @@
     show-default-actions
     confirm-text="Slet"
     cancel-text="Annuller"
+    class="z-[70]"
     @confirm="handleConfirmDelete"
     @cancel="handleCancelDelete"
     @close="handleCancelDelete"
